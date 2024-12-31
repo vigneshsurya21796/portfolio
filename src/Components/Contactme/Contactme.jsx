4b34f460bb5f9013cd61274c29c0966853028da7
Surya P <vigneshsurya21796@gmail.com>
2024-12-31 10:53:10 +0530

    Updated footer and header

--- src/Components/Contactme/Contactme.jsx
+++ src/Components/Contactme/Contactme.jsx
@@ -2,41 +2,80 @@ import React, { useState } from "react";
 import "./Contactme.css";
 
 function Contactme() {
-  const [form, setform] = useState({});
+  const [form, setform] = useState({
+    name: "",
+    email: "",
+    contactnumber: "",
+    message: "",
+  });
+  const formchange = (e) => {
+    const { name, value } = e.target;
+
+    setform((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
   return (
     <div className="contactme__container">
       <div className="contactme__containter-mail">
-        <div>Contact Me</div>
+        <h1>Contact Me</h1>
 
-        <div>
+        <p>
           You can write a mail directly to me if you want to work with me or
           collaborate for a project.
-        </div>
+        </p>
       </div>
       <div className="contactme__containter-form">
-        <form>
+        <form className="contactme__form-flex">
           <div className="contactme__input-container">
-            <label htmlFor="Name">Name:</label>
-            <input type="text" name="Name" id="Name" required />
-          </div>
-          <div className="contactme__input-container">
-            <label htmlFor="Email">Email:</label>
-            <input type="email" name="Email" id="Email" required />
-          </div>
-          <div className="contactme__input-container">
-            <label htmlFor="Contactnumber">Contactnumber:</label>
+            <label htmlFor="name">Name:</label>
             <input
-              type="number"
-              name="Contactnumber"
-              id="Contactnumber"
+              type="text"
+              name="name"
+              id="name"
+              required
+              value={form.name}
+              onChange={formchange}
+            />
+          </div>
+          <div className="contactme__input-container">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={form.email}
+              onChange={formchange}
+              required
+            />
+          </div>
+
+          <div className="contactme__input-container">
+            <label htmlFor="contactnumber">Contactnumber:</label>
+
+            <input
+              type="tel"
+              name="contactnumber"
+              id="contactnumber"
+              value={form.contactnumber}
+              onChange={formchange}
+              pattern="[0-9]{10}"
               required
             />
           </div>
           <div className="contactme__input-container">
-            <label htmlFor="Message">Message:</label>
-            <input type="text" name="" id="" required />
+            <label htmlFor="message">Message:</label>
+            <textarea
+              placeholder="Write you message"
+              name="message"
+              id="message"
+              required
+              value={form.message}
+              onChange={formchange}
+            />
           </div>
-          <div>
+          <div className="contacme__sendmessage">
             <button type="submit">Sendmessage</button>
           </div>
         </form>
